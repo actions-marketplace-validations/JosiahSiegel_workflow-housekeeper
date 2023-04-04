8855251dc6007c1a344da181f4cea44b3722ad3e
Josiah Siegel <josiah.siegel@semanticbits.com>
2023-04-04 15:39:01 -0400

    update github auth

--- lib/housekeeper.sh
+++ lib/housekeeper.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 # Init
-repo="$2"
-ignore_branch_workflows=$3
-
-# GitHub auth
-echo "$1" >auth.txt
-gh auth login --with-token <auth.txt
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
+repo="$1"
+ignore_branch_workflows=$2
 
 if [[ -z $ignore_branch_workflows ]]; then
   ignore_branch_workflows=false
   echo "ignore_branch_workflows: $ignore_branch_workflows"
 fi
 
+if [[ $ignore_branch_workflows = true ]]; then
+  files=$(ls -1 .github/workflows/ | sed  's/^/and .path != \".github\/workflows\//;s/$/\"/')
+else
+  files=""
+fi
+
 last_week=$(date -d "$date -1 weeks" +%s)
 runs="repos/$repo/actions/runs"
-files=$(ls -1 .github/workflows/ | sed  's/^/and .path != \".github\/workflows\//;s/$/\"/')
 query=".workflow_runs[] \
 | select( \
 .path != \".github/workflows/placeholder.yaml\" \
 "$files"
 )
-| select (.run_started_at | . == null or fromdateiso8601 < $last_week)
 | (.id)"
-gh api --paginate $runs --jq "$query" \
-| xargs -n1 -I % gh api $runs/%
+output=$(gh api --paginate $runs --jq "$query")
+
+echo "housekeeping_output=$(echo $output)" >> $GITHUB_OUTPUT
